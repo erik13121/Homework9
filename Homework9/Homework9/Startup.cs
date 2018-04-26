0c28f4f6b5da6b7732f0e023ce3071048ef1993d
Mr. E Grassman <u16087454@up.ac.za>
2018-04-26 10:15:51 +0200

    Added homework files of base program

---
+++ Homework9/Homework9/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Homework9.Startup))]
+namespace Homework9
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
